c62d61836b31d45f238214d1f35722aa3bb3b8ad
andrew2764 <andrew2764@gmail.com>
2024-02-07 15:33:34 +0800

    Try loading sdk to local scope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import { TrackingScripts } from "./tracking";
+import { useEffect } from "react";
+import { RealEyeSdk } from "https://app.realeye.io/sdk/js/testRunnerEmbeddableSdk-1.3.js";
 
+let reSdk: RealEyeSdk;
 export default function Home() {
+  useEffect(() => {
+    import(
+      /* webpackIgnore: true */ "https://app.realeye.io/sdk/js/testRunnerEmbeddableSdk-1.3.js"
+    ).then(({EmbeddedPageSdk}) => {
+      reSdk = new EmbeddedPageSdk(false)
+    })
+  }, [])
   return (
     <main>
       <h1>Lorem, ipsum dolor sit.</h1>
-      <TrackingScripts />
+      {/* <TrackingScripts /> */}
     </main>
   );
 }
